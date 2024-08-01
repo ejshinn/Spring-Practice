d5ce7ce7de7fff60fe89ea4e4a7105aae8d98a7c
ejshinn <ejshin.dev@gmail.com>
2024-08-02 00:18:27 +0900

    REST 방식 : service, mapper, xml, view 파일 작성

---
+++ board3/src/main/java/bitc/fullstack405/board3/dto/BoardDTO.java
@@ -0,0 +1,15 @@
+package bitc.fullstack405.board3.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDTO {
+    private int boardIdx;
+    private String title;
+    private String content;
+    private int hitCnt;
+    private String createDate;
+    private String createUser;
+    private String updateDate;
+    private String updateUser;
+}

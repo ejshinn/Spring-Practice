319b68e5a40e07be98ecef62bb2f65329d8f0d33
ejshinn <ejshin.dev@gmail.com>
2024-11-20 23:53:15 +0900

    no message

---
+++ security/src/test/java/bitc/fullstack405/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

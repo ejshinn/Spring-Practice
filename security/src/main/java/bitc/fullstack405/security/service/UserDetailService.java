2e78093986e57e351d4f2fb5770a8e39d3745d99
ejshinn <ejshin.dev@gmail.com>
2024-11-21 02:20:36 +0900

    사용자 인증을 위한 사용자 정보 조회 서비스 클래스

---
+++ security/src/main/java/bitc/fullstack405/security/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package bitc.fullstack405.security.service;
+
+import bitc.fullstack405.security.database.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email + "에 해당하는 사용자가 없습니다."));
+    }
+}

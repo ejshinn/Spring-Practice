3566a6a9dd2823bd310738deb7eeb6af1445dcc5
ejshinn <ejshin.dev@gmail.com>
2025-01-10 01:56:49 +0900

    JwtTokenService 생성

---
+++ jwtServer/src/main/java/com/bitc/jwtserver/service/JwtTokenService.java
@@ -0,0 +1,11 @@
+package com.bitc.jwtserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JwtTokenService {
+
+
+}

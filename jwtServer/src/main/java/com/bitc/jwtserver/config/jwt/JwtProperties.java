f1aba81110c121d7b3a301a2655031da88af8de5
ejshinn <ejshin.dev@gmail.com>
2025-01-10 17:42:03 +0900

    JWT 설정

---
+++ jwtServer/src/main/java/com/bitc/jwtserver/config/jwt/JwtProperties.java
@@ -0,0 +1,17 @@
+package com.bitc.jwtserver.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+// JWT 사용 시 application.property에 있는 설정을 가져오기 위한 클래스
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+
+    private String issuer;
+    private String secretKey;
+}
